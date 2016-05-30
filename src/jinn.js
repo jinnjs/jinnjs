9499eb622ae46658dd1c754c74f756730cf1b7bc
Dmitry Balakov <balakovd@gmail.com>
2016-05-30 06:50:46 +0300

    Jinn.parse start

--- src/jinn.js
+++ src/jinn.js
@@ -19,6 +19,57 @@ class Jinn {
         this.services = new Services(this);
         this.environment = new Environment(this);
     }
+
+    /**
+     * Execute task
+     * @func execute
+     * @param {string} description -  task description
+     * @return {Promise} promise - promise
+     */
+    execute(description) {
+
+    }
+
+    /**
+     * Execute task
+     * @func parse
+     * @param {string} description -  task description
+     * @return {Object} - return description { task : String, args : Object, postprocessors : Object }
+     */
+    parse(description) {
+        var result = { args : {}, postprocessors : {} };
+        var state = { type : 'task' };
+        description.split(SEPARATOR).forEach((key)=> {
+            switch(state.type) {
+                case 'task':
+                    result.task = key;
+                    state.type = 'none';
+                    return;
+
+                case 'none':
+                    if (key.substr(0, 2) === '--') {
+                        state.type = 'postprocessors';
+                        state.key = key.substr(2);
+                        return;
+                    }
+                    if (key.substr(0, 1) === '-') {
+                        state.type = 'args';
+                        state.key = key.substr(1);
+                        return;
+                    }
+                    break;
+
+                case 'args':
+                case 'postprocessors':
+                    result[state.type][state.key] = key;
+                    state.type = 'none';
+                    return;
+            }
+        });
+        return result;
+    }
 }
+
+const SEPARATOR = ' ';
 
 module.exports = Jinn;
--- test/jinn.js
+++ test/jinn.js
@@ -16,4 +16,17 @@ describe('Jinn', ()=> {
         assert.equal(jinn.tasks._jinn, jinn, 'jinn.tasks._jinn');
         assert.equal(jinn.services._jinn, jinn, 'jinn.services._jinn');
     });
+
+    it('execute', ()=> {
+
+    });
+
+    it('parse', ()=> {
+        var jinn = new Jinn();
+
+        assert.deepEqual(
+            jinn.parse('t:task -a1 file -a22s sdd --name ResultName'),
+            { task : 't:task', args: { a1: 'file', a22s : 'sdd' }, postprocessors : { name : 'ResultName' } },
+            'Parse result without "');  
+    });
 });
